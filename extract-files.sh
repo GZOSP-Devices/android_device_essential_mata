15564d0659928734ec8d2a5f478c2920651dced4
Pyrtle93 <xxpyrtlexx93@gmail.com>
2017-12-03 15:44:54 -0500

    more Validus updates

--- extract-files.sh
+++ extract-files.sh
@@ -25,9 +25,9 @@ VENDOR=essential
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+VALIDUS_ROOT="$MY_DIR"/../../..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$VALIDUS_ROOT"/vendor/validus/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -51,7 +51,7 @@ else
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$VALIDUS_ROOT"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC"
 
