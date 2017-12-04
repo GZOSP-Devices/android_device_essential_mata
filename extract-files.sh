0a76b0245fd8c74ca4f15fed109beeba45dae5dc
Pyrtle93 <xxpyrtlexx93@gmail.com>
2017-12-04 00:11:01 -0500

    Validus

--- extract-files.sh
+++ extract-files.sh
@@ -27,7 +27,7 @@ if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
 VALIDUS_ROOT="$MY_DIR"/../../..
 
-HELPER="$GZOSP_ROOT"/vendor/gzosp/build/tools/extract_utils.sh
+HELPER="$VALIDUS_ROOT"/vendor/validus/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -51,7 +51,7 @@ else
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$GZOSP_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$VALIDUS_ROOT"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC"
 
